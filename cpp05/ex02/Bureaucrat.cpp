3bc2bb70c8b1851f20d103fd69ecdcc478da2536
kuklunski <h.witch77@gmail.com>
2026-06-11 16:48:26 +0100

    Added pdf subjects

--- cpp05/ex02/Bureaucrat.cpp
+++ cpp05/ex02/Bureaucrat.cpp
@@ -59,11 +59,15 @@ int Bureaucrat::getGrade() const
 
 void Bureaucrat::incrementGrade()
 {
+    if (this->grade <= 1)
+        throw GradeTooHighException();
     grade--;
 }
 
 void Bureaucrat::decrementGrade()
 {
+    if (this->grade >= 150)
+        throw GradeTooLowException();
     grade++;
 }
 
